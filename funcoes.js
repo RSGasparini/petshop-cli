4769479814b24cf6c40f97b25384d32776662691
RSGasparini <rafaelgasparini.gig@gmail.com>
2022-07-04 20:59:06 -0300

    meu primeiro commit

--- funcoes.js
+++ funcoes.js
@@ -1,9 +1,41 @@
 const cachorros = require('./database/cachorros.json');
+const fs = require('fs');
+const path = require('path');
 
-function salvar(){}
+function salvar(){
 
-function buscar(id){}
+    let arquivo = path.resolve('./database/cachorros.json');
+    let json = JSON.stringify(cachorros, null, 4);
+    
+    fs.writeFileSync(arquivo, json);
+}
+salvar();
+
+// function buscar(id){
+//   let mesmoId = cachorros.id = id ? true : false;
+//   console.log(cachorros.id);
+  
+// }
+// return cachorros.find(mesmoId);
+(codigoBuscado) => {
+
+  function Buscar(id){
+      if(cachorros.id == codigoBuscado){
+          return true;
+      } else {
+          return false;
+      }
+  }
+
+  return cachorros.find(codigoBuscado);
+  
+  Buscar(1);
+}
+
 
 module.exports = {
     
+  salvar,
+  buscar,
+
 }
